bfc4930086c05db212248f5697da5b92d12c8313
gamingumar <umaraamer@gmail.com>
2020-01-08 21:45:22 +0500

    padder added

---
+++ src/Components/Padder.tsx
@@ -0,0 +1,42 @@
+/*
+ * File: Padder.tsx
+ * Project: GU RN Starter Kit
+ * File Created: Wednesday, 8th January 2020 9:44:44 pm
+ * Author: Umar Aamer (umaraamer@gmail.com)
+ * -----
+ * Last Modified: Wednesday, 8th January 2020 9:45:11 pm
+ * -----
+ * Copyright 2020 - 2020 WhileGeek, https://umar.tech
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import { vs, s } from 'react-native-size-matters';
+
+interface IPadder {
+  height?: number;
+  width?: number;
+  children?: any;
+}
+// A component for padding
+export const Padder = (props: IPadder) => {
+
+  let {height, width} = props;
+
+  if (!height) {
+    height = 5
+  }
+  if (!width) {
+    width = 0
+  }
+
+  const paddingVertical = vs(height) / 2;
+  const paddingHorizontal = s(width) / 2;
+  
+  return <View style={{paddingVertical, paddingHorizontal}}>{props.children}</View>;
+};
+
+Padder.defaultProps = {
+  height: 5,
+  width: 0,
+};
